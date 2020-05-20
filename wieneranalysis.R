ee8d615f8a8eeac604b79cca87e2f55111a666bc
alantump <46488060+alantump@users.noreply.github.com>
2020-05-20 20:37:13 +0200

    Update wieneranalysis.R

--- wieneranalysis.R
+++ wieneranalysis.R
@@ -13,7 +13,7 @@ library(reshape2)
 runApp(list(
   ui=fluidPage(    shinyjs::useShinyjs(),
                     headerPanel("Drift diffusion parameters"),
-                   sidebarPanel(sliderInput("ndt_value", label = "Non decision time", min=0.1,max=1, value=0.1, step=0.1),
+                   sidebarPanel(sliderInput("ndt_value", label = "Non decision time wiwne", min=0.1,max=1, value=0.1, step=0.1),
                                 sliderInput("bias_value", label = "Bias", min=0.1,max=0.9, value=0.5, step=0.05),
                    sliderInput("drift_value", label = "Drift", min=-2,max=2, value=0, step=0.1),
                    sliderInput("bs_value", label = "Boundary seperation", min=0.1,max=5, value=2, step=0.2),
